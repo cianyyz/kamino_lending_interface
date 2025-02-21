4c35b54332140fe8cc6e1a10101335269cdc1095
Cian <25750319+cianyyz@users.noreply.github.com>
2025-02-21 11:19:02 -0500

    Updated program ID to remove placeholder

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-solana_program::declare_id!("TH1S1SNoTAVAL1DPUBKEYDoNoTUSE11111111111111");
+solana_program::declare_id!("KLend2g3cP87fffoy8q1mQqGKjrxjC8boSyAYavgmjD");
 pub mod accounts;
 pub use accounts::*;
 pub mod typedefs;
